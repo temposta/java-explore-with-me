918ac646ec0d0b69f64233abd8c72f80c0941717
Sergey Kovalevich <temposta@yandex.ru>
2025-06-08 19:50:56 +0300

    фикс

--- evm-main-service/src/main/java/ru/practicum/evmservice/mainservice/MainServiceApplication.java
+++ evm-main-service/src/main/java/ru/practicum/evmservice/mainservice/MainServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan({"ru.practicum.evm"})
+@ComponentScan({"ru.practicum"})
 public class MainServiceApplication {
 
     public static void main(String[] args) {
